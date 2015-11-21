6cb34b679460c5741be7ead78279007a567b56d6
rober <rmh.dev@gmail.com>
2015-11-21 13:59:55 +0100

    Add order object to model

---
+++ src/Redsys/Model/Order.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of the Redsys package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT License
+ */
+
+namespace Redsys\Model;
+
+class Order implements ModelInterface
+{
+    private $value;
+
+    /**
+     * Order constructor.
+     * @param mixed $value
+     */
+    public function __construct($value)
+    {
+        $value = str_replace(" ", "", trim($value));
+        $this->value = (string)$value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function __toString()
+    {
+        return $this->getValue();
+    }
+}
---
+++ tests/Redsys/Tests/Model/OrderTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the Redsys package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT License
+ */
+
+namespace Redsys\Tests\Model;
+
+use Redsys\Model\Order;
+
+class OrderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetValueShouldReturnStringValue()
+    {
+        $order = new Order("1234aBc123");
+
+        $this->assertEquals("1234aBc123", $order->getValue());
+    }
+
+    /**
+     * @dataProvider orderValues
+     */
+    public function testOrderShouldCleanReceivedValue($expected, $code)
+    {
+        $merchantCode = new Order($code);
+
+        $this->assertEquals($expected, $merchantCode->getValue());
+    }
+
+    public function orderValues()
+    {
+        return array(
+            array("1234aBc123", "123 4a Bc123"),
+            array("1234aBc123", "\t1234aBc123\n"),
+        );
+    }
+
+    public function testToStringShouldReturnStringValue()
+    {
+        $order = new Order(4567890);
+
+        $this->assertEquals("4567890", (string)$order);
+    }
+}
