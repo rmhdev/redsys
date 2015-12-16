b9048b7692af8a9a10a124ed4e45c2c1a00bfb93
rober <rmh.dev@gmail.com>
2015-12-16 12:28:04 +0100

    Notification return authentication and parameter bag

--- src/Redsys/Payment/Redirect/Notification.php
+++ src/Redsys/Payment/Redirect/Notification.php
@@ -10,19 +10,34 @@
 
 namespace Redsys\Payment\Redirect;
 
+use Redsys\ParameterBag\Response;
+use Redsys\Security\Authentication\AuthenticationFactory;
+use Redsys\Security\Authentication\AuthenticationInterface;
+
 class Notification
 {
+    const VERSION = "Ds_SignatureVersion";
+    const PARAMETERS = "Ds_MerchantParameters";
+    const SIGNATURE = "Ds_Signature";
+
     /**
      * @var array
      */
     private $parameters;
 
     /**
-     * @param array $parameters
+     * @var string
      */
-    public function __construct($parameters = array())
+    private $key;
+
+    /**
+     * @param array $parameters
+     * @param string $key
+     */
+    public function __construct($parameters = array(), $key = "")
     {
         $this->parameters = is_array($parameters) ? $parameters : (array)$parameters;
+        $this->key = $key;
     }
 
     /**
@@ -31,5 +46,34 @@ class Notification
     public function toArray()
     {
         return $this->parameters;
+    }
+
+    /**
+     * @return AuthenticationInterface
+     */
+    public function getAuthentication()
+    {
+        $version = $this->getValue(self::VERSION);
+
+        return AuthenticationFactory::create($version, $this->key);
+    }
+
+    private function getValue($name, $default = null)
+    {
+        if (!array_key_exists($name, $this->parameters)) {
+            return $default;
+        }
+
+        return $this->parameters[$name];
+    }
+
+    /**
+     * @return Response
+     */
+    public function getParameterBag()
+    {
+        $authentication = $this->getAuthentication();
+
+        return new Response($authentication->decode($this->getValue(self::PARAMETERS, "")));
     }
 }
--- tests/Redsys/Tests/Payment/Redirect/NotificationTest.php
+++ tests/Redsys/Tests/Payment/Redirect/NotificationTest.php
@@ -10,7 +10,10 @@
 
 namespace Redsys\Tests\Payment\Redirect;
 
+use Redsys\ParameterBag\Response;
 use Redsys\Payment\Redirect\Notification;
+use Redsys\Security\Authentication\AuthenticationFactory;
+use Redsys\Security\Authentication\HmacSha256V1;
 
 class NotificationTest extends \PHPUnit_Framework_TestCase
 {
@@ -37,5 +40,63 @@ class NotificationTest extends \PHPUnit_Framework_TestCase
         $notification = new Notification("test");
 
         $this->assertEquals(array("test"), $notification->toArray());
+    }
+
+    public function testGetAuthenticationShouldReturnObject()
+    {
+        $expected = $this->createAuthentication();
+        $notificationValues = array(
+            "Ds_SignatureVersion" => $expected->getName(),
+            "Ds_MerchantParameters" => "lorem_ipsum",
+            "Ds_Signature" => "1234",
+        );
+        $notification = new Notification($notificationValues, $this->secretKey());
+
+        $this->assertEquals($expected, $notification->getAuthentication());
+    }
+
+    private function createAuthentication()
+    {
+        return AuthenticationFactory::create(HmacSha256V1::NAME, $this->secretKey());
+    }
+
+    private function secretKey()
+    {
+        return "Mk9m98IfEblmPfrpsawt7BmxObt98Jev";
+    }
+
+    public function testGetParameterBagShouldReturnObject()
+    {
+        $authentication = $this->createAuthentication();
+        $parameterBag = new Response($this->getNotificationParameters());
+        $notificationValues = array(
+            "Ds_SignatureVersion" => $authentication->getName(),
+            "Ds_MerchantParameters" => $authentication->encode($parameterBag->all()),
+            "Ds_Signature" => "1234",
+        );
+        $notification = new Notification($notificationValues);
+
+        $this->assertEquals($parameterBag, $notification->getParameterBag());
+    }
+
+    private function getNotificationParameters()
+    {
+        return array(
+            "Ds_Amount" => "10025",
+            "Ds_AuthorisationCode" => "123456",
+            "Ds_Card_Country" => "724",
+            "Ds_Card_Type" => "D",
+            "Ds_ConsumerLanguage" => "001",
+            "Ds_Currency" => "978",
+            "Ds_Date" => "25/11/2015",
+            "Ds_Hour" => "19:30",
+            "Ds_MerchantCode" => "1234abcde",
+            "Ds_MerchantData" => "Lorem Ipsum",
+            "Ds_Order" => "1234qwerty",
+            "Ds_Response" => "80",
+            "Ds_SecurePayment" => "1",
+            "Ds_TransactionType" => "0",
+            "Ds_Terminal" => "001",
+        );
     }
 }
